8fe105cf5e202af10db462c33b58aef80801ac23
Steven Paulino <stevenpaulino1@hotmail.com>
2019-09-09 21:17:23 -0400

    Updates

---
+++ precache-manifest.48aec2f96ec619b59a71aa49715d755a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5773bc4feec688c1006b57095180c263",
+    "url": "/knock-lights-out/index.html"
+  },
+  {
+    "revision": "f8a9a6d6e77fb860046f",
+    "url": "/knock-lights-out/static/css/main.0a4fed35.chunk.css"
+  },
+  {
+    "revision": "4b5ddf3d93b836d85a10",
+    "url": "/knock-lights-out/static/js/2.f30ee633.chunk.js"
+  },
+  {
+    "revision": "f8a9a6d6e77fb860046f",
+    "url": "/knock-lights-out/static/js/main.be4102e4.chunk.js"
+  },
+  {
+    "revision": "e986d01bf00f593bcd9d",
+    "url": "/knock-lights-out/static/js/runtime~main.bd245cb0.js"
+  }
+]);
